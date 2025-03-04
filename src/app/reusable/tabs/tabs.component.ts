05df2b6f717afa7cbeec9c8559fc3015d7501d71
limlei2 <87996616+limlei2@users.noreply.github.com>
2025-03-04 02:35:54 -0500

    input and output

---
+++ src/app/reusable/tabs/tabs.component.ts
@@ -0,0 +1,31 @@
+import { NgClass } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs',
+  imports: [NgClass],
+  templateUrl: './tabs.component.html',
+  styleUrl: './tabs.component.css'
+})
+export class TabsComponent implements OnInit {
+
+  @Input() tabList: string[] = [];
+
+  @Output() onTabClicked = new EventEmitter<string>();
+
+  currentTab: string = '';
+
+  constructor(){
+    
+  }
+
+  ngOnInit(): void {
+    this.currentTab = this.tabList[0];
+  }
+
+  onTabChange(tabName: string) {
+    this.currentTab = tabName;
+    this.onTabClicked.emit(tabName)
+  }
+
+}
